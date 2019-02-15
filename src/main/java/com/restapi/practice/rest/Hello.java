e57343f4238bc2c2389719d5187bea390de6986c
shijnasurendran <47676285+shijnasurendran@users.noreply.github.com>
2019-02-15 17:08:03 -0500

    Hello class added

---
+++ src/main/java/com/restapi/practice/rest/Hello.java
@@ -0,0 +1,15 @@
+package com.restapi.practice.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Hello {
+
+    @RequestMapping("/")
+    public String s(){
+        return "hello from shijna";
+    }
+
+}
